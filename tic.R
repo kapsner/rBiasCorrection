81ca0195cfeb2208cc5d0a5ac856d6bb78eed1ad
kapsner <lorenz.kapsner@web.de>
2020-07-18 13:02:57 +0200

    Update tic.R

--- tic.R
+++ tic.R
@@ -1,5 +1,5 @@
 # installs dependencies, runs R CMD check, runs covr::codecov()
 do_package_checks()
 
-get_stage("script") %>%
-    add_code_step(install.packages("rmarkdown"))
+get_stage("install") %>%
+    add_code_step(remotes::update_packages(packages = "rmarkdown", upgrade = "always"))
